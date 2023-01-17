16f475c29c6622c9b635d6b327ba573c58399748
aneeshchali <chali.aneesh@gmail.com>
2023-01-17 20:14:03 +0530

    upto 41:16

---
+++ src/utilities/formatCurrency.ts
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: "INR",
+  style: "currency",
+});
+
+const formatCurrency = (number: number) => {
+  return CURRENCY_FORMATTER.format(number);
+};
+
+export default formatCurrency;
